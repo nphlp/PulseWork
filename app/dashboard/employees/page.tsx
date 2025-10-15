d25d0eb6ec1839f4075d9e9637b635cd346da2bf
nansphilip <nansphilip@gmail.com>
2025-10-15 17:07:01 +0200

    feat: migration user role step 1

--- app/dashboard/employees/page.tsx
+++ app/dashboard/employees/page.tsx
@@ -1,3 +1,7 @@
-export default function Page() {
+// import { UserFindManyAction } from "@actions/UserAction";
+
+export default async function Page() {
+    // const employees = await UserFindManyAction({ where: { role: "ADMIN" } });
+
     return <div>Dashboard Employees</div>;
 }
