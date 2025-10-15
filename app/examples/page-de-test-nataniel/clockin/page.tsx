599c291ba82dad6cdf9d63702d6b0a29bd28b7c8
natkrf <149679599+natkrf@users.noreply.github.com>
2025-10-15 15:25:55 +0200

    feat:  test clock page
    * feat: add test page of nataniel
    
    * feat: href modify for test page
    
    * feat: remove send email

---
+++ app/examples/page-de-test-nataniel/clockin/page.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+export default function Page() {
+    const router = useRouter();
+
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            router.back();
+        }, 5000);
+
+        return () => clearTimeout(timeout);
+    }, [router]);
+
+    return (
+        <main className="bg-white dark:bg-black">
+            <h1 className="text-5xl font-semibold text-black dark:text-white">Clock-in effectué avec succès ⏰ </h1>
+            <p className="align-text-bottom text-blue-400 dark:text-blue-400">
+                Redirection automatique dans 5 secondes...
+            </p>
+        </main>
+    );
+}
